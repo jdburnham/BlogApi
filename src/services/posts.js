fb6529b5431930011f01545060c73320548496dd
jdburnham <jburnham@stu.southtexascollege.edu>
2020-04-11 15:40:41 -0500

    Added Auth

--- src/services/posts.js
+++ src/services/posts.js
@@ -4,43 +4,52 @@ import db from "../helpers/db";
 const postSchema = Joi.object({
   title: Joi.string().required(),
   text: Joi.string().required(),
-  author: Joi.string().required()
+  author: Joi.string().required(),
 });
 
-const createNewPostObject = data => {
+const createNewPostObject = (data) => {
   const { error, value } = postSchema.validate(data);
   return !error
     ? {
         ...value,
         created: new Date().toLocaleString(),
-        updated: new Date().toLocaleString()
+        updated: new Date().toLocaleString(),
       }
     : { error };
 };
 
-const createPostObject = data => {
+const createPostObject = (data) => {
   const { error, value } = postSchema.validate(data);
   return !error
     ? {
         ...value,
-        updated: new Date().toLocaleString()
+        updated: new Date().toLocaleString(),
       }
     : { error };
 };
 
 export const getAll = async () => {
   const posts = await db("posts");
-  return posts;
+  const postsPromises = posts.map(async (post) => {
+    const author = await db("users").where({ id: post.author }).first();
+    post.author = author;
+    return post;
+  });
+
+  return await Promise.all(postsPromises);
 };
 
-export const getById = async id => {
-  const post = await db("posts")
-    .where({ id })
-    .first();
-  return post;
+export const getById = async (id) => {
+  const post = await db("posts").where({ id }).first();
+  if (post) {
+    const author = await db("users").where({ id: post.author }).first();
+    return { ...post, author };
+  } else {
+    return null;
+  }
 };
 
-export const add = async post => {
+export const add = async (post) => {
   const postObject = createNewPostObject(post);
   if (!postObject.error) {
     const id = await db("posts").insert(post);
@@ -54,9 +63,7 @@ export const update = async (post, id) => {
   if (await getById(id)) {
     const postObject = createPostObject(post);
     if (!postObject.error) {
-      await db("posts")
-        .where({ id })
-        .update(postObject);
+      await db("posts").where({ id }).update(postObject);
       return await getById(id);
     } else {
       return postObject;
@@ -66,9 +73,7 @@ export const update = async (post, id) => {
   }
 };
 
-export const remove = async id => {
-  await db("posts")
-    .where({ id })
-    .del();
+export const remove = async (id) => {
+  await db("posts").where({ id }).del();
   return { msg: "Ok" };
 };
